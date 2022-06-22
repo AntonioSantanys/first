73d12c2922f8b1887163be7a10191a3f9df635a5
Antônio Santana <antonionetosg6@gmail.com>
2022-06-21 22:02:54 -0300

    Adicionado

---
+++ Fundamentos/04-Introducao-JavaScript/4.1-exercício/exe5/script.js
@@ -0,0 +1,15 @@
+let pessoa1 = "lista";
+
+switch (pessoa1) {
+
+    case "aprovada":
+        console.log("Parabéns você foi aprovado(a)!");
+        break;
+
+    case "reprovada":
+        console.log("Você foi reprovado.");
+        break;
+    case "lista":
+        console.log("Você está na nossa lista de espera, aguarde :)");
+        break;
+}
