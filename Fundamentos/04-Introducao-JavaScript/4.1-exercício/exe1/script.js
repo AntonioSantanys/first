7767800ce7625e71cc98d822cf5546568de58bc4
Antônio Santana <antonionetosg6@gmail.com>
2022-06-21 21:28:55 -0300

    Adição de exercício JavaScript

---
+++ Fundamentos/04-Introducao-JavaScript/4.1-exercício/exe1/script.js
@@ -0,0 +1,12 @@
+const name = "Antônio Santana";
+const birthCity = "Teresina";
+let birthYear = 2004;
+
+birthYear = 2030;
+
+//Não será possível alterar, pois birthCity está definido como constante
+birthCity = "Fortaleza"
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
---
+++ Fundamentos/04-Introducao-JavaScript/4.1-exercício/exe3/script.js
@@ -0,0 +1,12 @@
+const nota1 = 60;
+
+if (nota1 >= 80) {
+
+    console.log("Parabéns, você foi aprovado(a)")
+} else if (nota1 >= 60 && nota1 < 80) {
+
+    console.log("Você está na nossa lista de espera")
+} else {
+
+    console.log("Você foi reprovado(a)")
+}
