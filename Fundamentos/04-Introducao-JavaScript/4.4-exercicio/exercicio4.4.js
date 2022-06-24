9a6a70e56bbb3e9f8c864687b380ab4fd5f172b9
Antônio Santana <antonionetosg6@gmail.com>
2022-06-24 18:48:51 -0300

    Q5 solved

--- Fundamentos/04-Introducao-JavaScript/4.4-exercicio/exercicio4.4.js
+++ Fundamentos/04-Introducao-JavaScript/4.4-exercicio/exercicio4.4.js
@@ -1,15 +1,30 @@
 // Usando o objeto abaixo, faça os exercícios a seguir:
 
-//4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. 
+//5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes 
+// valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. 
+//Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. 
 
-let info = { 
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  recorrente: 'sim'  
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
 };
 
-for(key in info) {
- 
-   console.log(info[key])
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+};
+
+for (let key in info){
+
+     if(key === 'recorrente' && info[key] === 'Sim' && info[key] === 'Sim') {
+
+        console.log('Ambos recorrentes')
+     } else {
+        console.log(info[key] + ' e ' + info2[key])
+     }
+
 }
