b164ca7f9e2ed2dd4627648f91d652f32a42be83
Antônio Santana <antonionetosg6@gmail.com>
2022-06-23 21:32:44 -0300

    Exercicios solved

--- Fundamentos/04-Introducao-JavaScript/4.3-exercicio/exe1.js
+++ Fundamentos/04-Introducao-JavaScript/4.3-exercicio/exe1.js
@@ -6,8 +6,8 @@ let fatorial = 1;
 
 for (let index = 1; index <= 10; index += 1) {
 
-    fatorial *= index;
-    console.log(fatorial)
+  fatorial *= index;
+  console.log(fatorial)
 }
 
 console.log('\n', '+------ Fim do exercício 2 ------+');
@@ -26,23 +26,30 @@ console.log('\n', '+------ Fim do exercício 2 ------+')
 
 // 3- Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
 
+console.log('\n', '+------ Início do exercício 3 ------+');
+
 let array = ['java', 'javascript', 'python', 'html', 'css'];
 
-let biggestWord = array[0];
-let smallestWord = array[0];
+let maiorPalavra = [0];
+let menorPalavra = [0];
 
 for (let index = 0; index < array.length; index += 1) {
-  if (array[index].length > biggestWord.length) {
-    biggestWord = array[index];
+
+  if (array[index].length > maiorPalavra.length) {
+
+    maiorPalavra = array[index];
   }
 }
 
 for (let index = 0; index < array.length; index += 1) {
-  if (array[index].length < smallestWord.length) {
-    smallestWord = array[index];
+
+  if (array[index].length < maiorPalavra.length) {
+
+    menorPalavra = array[index]
   }
 }
 
-console.log(biggestWord);
-console.log(smallestWord);
+console.log(maiorPalavra);
+console.log(menorPalavra);
 
+console.log('\n', '+------ Fim do exercício 3 ------+')
