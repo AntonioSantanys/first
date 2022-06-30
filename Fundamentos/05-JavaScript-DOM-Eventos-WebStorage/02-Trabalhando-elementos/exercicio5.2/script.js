c1cb6785fb6bde1537e8e7941a0ac4f8b8ec5ef7
Antônio Santana <antonionetosg6@gmail.com>
2022-06-30 11:04:08 -0300

    Q2 ~ Q3 solved

--- Fundamentos/05-JavaScript-DOM-Eventos-WebStorage/02-Trabalhando-elementos/exercicio5.2/script.js
+++ Fundamentos/05-JavaScript-DOM-Eventos-WebStorage/02-Trabalhando-elementos/exercicio5.2/script.js
@@ -2,4 +2,14 @@ let elementH1 = document.createElement('hi');
 elementH1.innerText = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(elementH1);
 
-console.log(corpo);
+
+let elementMain = document.createElement('main');
+elementMain.className = 'main-content';
+document.body.appendChild(elementMain);
+
+
+let filhoElementMain = document.createElement('section');
+elementMain.appendChild(filhoElementMain)
+
+console.log(document.body.childNodes);
+
