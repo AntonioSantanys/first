09b7479eaf09c3c20f32595c6f30dd30e2eeb847
Antônio Santana <antonionetosg6@gmail.com>
2022-06-30 11:28:45 -0300

    Q3 ~ Q9 solved

--- Fundamentos/05-JavaScript-DOM-Eventos-WebStorage/02-Trabalhando-elementos/exercicio5.2/script.js
+++ Fundamentos/05-JavaScript-DOM-Eventos-WebStorage/02-Trabalhando-elementos/exercicio5.2/script.js
@@ -9,7 +9,46 @@ document.body.appendChild(elementMain);
 
 
 let filhoElementMain = document.createElement('section');
-elementMain.appendChild(filhoElementMain)
+elementMain.appendChild(filhoElementMain).className = 'center-content';
 
-console.log(document.body.childNodes);
+
+let paragraphFilhoElementMain = document.createElement('p');
+filhoElementMain.appendChild(paragraphFilhoElementMain).innerText = 'Paragraph Element Main';
+
+let segundoFilhoElementMain = document.createElement('section');
+elementMain.appendChild(segundoFilhoElementMain).className = 'left-content';
+
+
+let terceiroFilhoElementMain = document.createElement('section0');
+elementMain.appendChild(terceiroFilhoElementMain).className = 'right-content';
+
+
+let filhoImageSegundoFIlhoElementMain = document.createElement('img');
+segundoFilhoElementMain.appendChild(filhoElementMain).className = 'small-image';
+filhoImageSegundoFIlhoElementMain.src = 'https://picsum.photos/200';
+
+
+let elementListaRightContentUl = document.createElement('ul');
+terceiroFilhoElementMain.appendChild(elementListaRightContentUl);
+
+let numeros = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'];
+
+for(let i in numeros){
+   let elementLi = document.createElement('li');
+   elementLi.innerHTML = numeros[i];
+   elementListaRightContentUl.appendChild(elementLi);
+};
+
+
+for(let i = 0; i <= 2; i += 1){
+
+   let elementH3 = document.createElement('h3');
+   elementH3.innerHTML = 'Títulos aqui';
+   elementMain.appendChild(elementH3);
+
+};
+
+
+
+
 
