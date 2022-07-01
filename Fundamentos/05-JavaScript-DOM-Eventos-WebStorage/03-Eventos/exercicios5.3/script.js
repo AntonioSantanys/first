2e9b702ca892141f9152d64d4e3d4f5a1aacff7c
Antônio Santana <antonionetosg6@gmail.com>
2022-07-01 11:52:56 -0300

    Adicionado exercicio 5.4

--- Fundamentos/05-JavaScript-DOM-Eventos-WebStorage/03-Eventos/exercicios5.3/script.js
+++ Fundamentos/05-JavaScript-DOM-Eventos-WebStorage/03-Eventos/exercicios5.3/script.js
@@ -13,4 +13,38 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-  // Escreva seu código abaixo.
+// Escreva seu código abaixo.
+
+function createDaysOfTheMonth() {
+    let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    let listDayDecember = document.getElementById('days');
+
+    for (let index = 0; index < decemberDaysList.length; index += 1){
+
+        let daysNumbers = decemberDaysList[index];
+        let dayNumberElement = document.createElement('li');
+        dayNumberElement.innerText = daysNumbers;
+        listDayDecember.appendChild(dayNumberElement);
+       
+        if(daysNumbers == 24 || daysNumbers == 31){
+             dayNumberElement.className = 'day holiday'
+             listDayDecember.appendChild(dayNumberElement);
+        } else if (daysNumbers == 4 || daysNumbers == 11 || daysNumbers == 18){
+             dayNumberElement.className = 'day friday';
+             listDayDecember.appendChild(dayNumberElement);
+        }else if (daysNumbers == 25){
+             dayNumberElement.className = 'day holiday friday';
+        } else {
+             dayNumberElement.className = 'day';
+             listDayDecember.appendChild(dayNumberElement);
+
+        }
+        
+    }
+
+    listDayDecember.className = 'day';
+
+
+}
+
+createDaysOfTheMonth();
